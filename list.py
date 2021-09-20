832f5d34d6d78642665df7ca7166280f33785712
suyogss11 <87109516+suyogss11@users.noreply.github.com>
2021-09-20 18:23:30 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,11 @@
+print("enter a element ")
+n = int(input("enter no of element "))
+
+for i in range(0,n):
+    
+    for elem in Input:
+        temp = []
+    for x in elem:
+        if x>0:
+            temp.append(x)
+    Output.append(temp)
